e18648169242e275b082edf9ca96f2a0f7c834e1
jpwebdev <jjpwebdev@gmail.com>
2017-12-05 14:00:29 -0500

    changes

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 (function () {
     const hangman = {
-        // VARIABLES
+        // VARIABLES //
         game_array : [ "armadillo", "dog", "fish", "horse", "snake", "otter", "spider", "bear", "cat", "lizard", "alligator", "eagle" ],
         g_a_length: function() { return hangman.game_array.length },
         score_win: 0,
@@ -15,33 +15,30 @@
             this.bind_events();
         },
 
-        // CACHE DOM
+        // CACHE DOM //
         cache_dom: function() {
             this.word_container = document.getElementById("word_container");
             this.letters_used = document.getElementById("letters_used");
         },
 
-        // EVENT BINDERS
-        bind_events: function() {
+        // EVENT BINDERS //
+        bind_events: function() {    
             window.addEventListener("keydown", this.get_letter);
         },
 
 
-        // FUNCTIONS
+        // FUNCTIONS //
 
-        // Get a new word to play
-            // Generate a random number between 0 and the length of the array, to use as index for the array
-        random_index : function() { 
+        random_index : function() {  // Generate a random number between 0 and the length of the array, to use as index for the array
             return Math.floor((Math.random() * this.g_a_length()))
         },
-            // Finds the new word... which is the array element based on the random index
-        word_play: function() {
+            
+        word_play: function() {  //Finds the new word... which is the array element based on the random index
             const game_word = this.game_array[this.random_index()];
             return game_word;
         },
 
-        // Show _ for every letter of the word to guess
-        under_generator: function () {
+        under_generator: function() {   // Show _ for every letter of the word to guess
             let word = this.word_play();
             let word_l = word.length;
 
@@ -51,14 +48,14 @@
                 this.underscore_arr.push("_");
             }
 
-            for(let i = 0; i < this.underscore_arr.length; i++) {
-                this.word_container.innerHTML = "Wa";
-            }
+            // for(let j = 0; j < this.underscore_arr.length; j++) {      
+            //     console.log(`This is *this.word_container* ${this.cache_dom().word_container}`);
+            //     //.innerHTML = "this.underscore_arr[i]";
+            // }
             console.log(this.underscore_arr);
         },
 
-        // Get the letter pressed by user
-        get_letter: (event) => {
+        get_letter: function(event) {   // Get the letter pressed by user
             let str = `Keyboard Event: key = ${event.key} and code = ${event.code}`;
             this.letters_used.innerHTML = str;
         }
